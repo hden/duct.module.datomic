15b40e57ed7f271e80478f28aaad67b29f4e8cb5
Haokang Den <haokang.den@gmail.com>
2021-01-10 12:56:50 +0900

    force new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hden/duct.module.datomic "0.1.0-SNAPSHOT"
+(defproject hden/duct.module.datomic "0.2.0-SNAPSHOT"
   :description "A Duct module that adds Integrant keys for a Datomic database connection and Ragtime migrations to a configuration."
   :url "https://github.com/hden/duct.module.datomic"
   :license {:name "EPL-2.0"
