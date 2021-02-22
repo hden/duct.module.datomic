2a0c705babeed5835a2dd01065562c6be6a14fb3
Haokang Den <haokang.den@gmail.com>
2021-02-23 01:21:36 +0900

    fix configs

--- src/duct/module/datomic.clj
+++ src/duct/module/datomic.clj
@@ -2,22 +2,13 @@
   (:require [duct.core :as core]
             [integrant.core :as ig]))
 
-(defn- get-environment [config options]
-  (:environment options (:duct.core/environment config :production)))
-
-(def ^:private logger-configs
-  {:production
-   {:duct.logger.timbre/cast {}
-    :duct.logger/timbre
-    ^:demote {:appenders ^:displace {:duct.logger.timbre/cast (ig/ref :duct.logger.timbre/cast)}}}})
-
 (defmethod ig/init-key :duct.module/datomic [_ options]
   (fn [config]
     (core/merge-configs
      config
-     {:duct.database/datomic options}
-     {:duct.migrator.ragtime/datomic
+     {:duct.database/datomic options
+      :duct.logger/cast options
+      :duct.migrator.ragtime/datomic
       ^:demote {:database   (ig/ref :duct.database/datomic)
                 :logger     (ig/ref :duct/logger)
-                :migrations []}}
-     (logger-configs (get-environment config options)))))
+                :migrations []}})))
